78597a789f48b848229e9be9e708c341da72de11
Ruben Estevao <ruben.slingshot@gmail.com>
2019-07-05 09:38:42 +0100

    add test database configuration

--- tests/Feature/AuthenticationTest.php
+++ tests/Feature/AuthenticationTest.php
@@ -3,13 +3,13 @@
 namespace Tests\Feature;
 
 use App\User;
-use Illuminate\Foundation\Testing\DatabaseTransactions;
+use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Support\Facades\Hash;
 use Tests\TestCase;
 
 class AuthenticationTest extends TestCase
 {
-    use DatabaseTransactions;
+    use RefreshDatabase;
 
     public function testUserCanLoginWithCorrectCredentials()
     {
